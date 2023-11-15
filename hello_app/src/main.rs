1223b01772e3d85b8b0e8be7ab0d310762892402
lispking <lispking@qq.com>
2023-11-15 09:12:58 +0800

    + step 4

--- hello_app/src/main.rs
+++ hello_app/src/main.rs
@@ -1,12 +1,24 @@
+#![feature(asm_const)]
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
 
+//const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    core::arch::asm!(
-        "wfi",
-        options(noreturn)
+    let arg0: u8 = b'C';
+    core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        add     t1, a7, t0
+        ld      t1, (t1)
+        jalr    t1
+        wfi",
+        abi_num = const SYS_PUTCHAR,
+        in("a0") arg0,
+        options(noreturn),
     )
 }
 
