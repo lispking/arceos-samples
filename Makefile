54f7895c1fbc8cdb7ea47defde3116c198655efa
lispking <lispking@qq.com>
2023-11-16 07:53:14 +0800

    + add hello_app_v2 and adjust scripts to support build more app

--- Makefile
+++ Makefile
@@ -1,7 +1,20 @@
 APP ?= hello_app
 
+PAYLOAD_PATH ?= $(shell pwd)/../arceos/payload
+PAYLOAD_BIN ?= apps.bin
+
 default:
-	cd $(APP) && $(MAKE)	
+	cd $(APP) && $(MAKE)
+
+	dd if=/dev/zero of=./$(PAYLOAD_BIN) bs=1M count=32
+	dd if=$(APP)/$(APP).bin of=./$(PAYLOAD_BIN) conv=notrunc
+
+	mkdir -p $(PAYLOAD_PATH)
+	cp ./$(PAYLOAD_BIN) $(PAYLOAD_PATH)/$(PAYLOAD_BIN)
+
+	xxd -ps $(APP)/$(APP).bin
 
 clean:
-	cd $(APP) && $(MAKE) clean
+	cd hello_app_v1 && $(MAKE) clean
+	cd hello_app_v2 && $(MAKE) clean
+	cd hello_app_v4 && $(MAKE) clean
---
+++ build_image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+APP_PATH=$(pwd)/../arceos/payload
+APP_BIN=apps.bin
+
+# build app to image
+build_image() {
+    local app_name=$1
+    local app_seek=$2
+
+    # Compile Apps
+    make APP=${app_name}
+
+    # Get App Size
+    if [ "$(uname)" == "Darwin" ]; then
+        local app_size=$(stat -f%z ./${app_name}/${app_name}.bin)
+    else
+        local app_size=$(stat -c%s ./${app_name}/${app_name}.bin)
+    fi
+
+    # Write App Size to App Bin
+    printf "$(printf '%04x' $app_size)" | xxd -ps | dd of=./${APP_BIN} conv=notrunc bs=1 seek=${app_seek}
+
+    # Write App to App Bin
+    local app_seek_file=$(($app_seek + 2))
+    dd if=./${app_name}/${app_name}.bin of=./${APP_BIN} conv=notrunc bs=1 seek=${app_seek_file}
+
+    echo "app_name: ${app_name} size: ${app_size} seek: from $app_seek to ${app_seek_file}"
+}
+
+# Create App Bin
+dd if=/dev/zero of=./${APP_BIN} bs=1M count=32
+
+# Write App Num to App Bin
+APP_NUM=2
+printf "$(printf '%02x' ${APP_NUM})" | xxd -ps | dd of=./${APP_BIN} conv=notrunc bs=1 seek=0
+
+build_image hello_app_v1 1
+build_image hello_app_v2 9
+
+mkdir -p ${APP_PATH}
+mv ./${APP_BIN} ${APP_PATH}/${APP_BIN}
+echo "build image: ${APP_PATH}/${APP_BIN} done"
